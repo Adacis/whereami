24f1da9854eb5756323b9f06e3f04bf52482eefa
Benjamin <benjamin@cybercorp.fr>
2022-06-08 15:53:43 +0200

    Helper

---
+++ templates/content/modal.php
@@ -0,0 +1,11 @@
+<div id="helper" class="modal">
+	<div class="modal-content">
+		<span class="modalClose">&times;</span>
+		<h2><?php p($l->t('How that work ?')); ?></h2>
+		<hr/>
+		<p><b><?php p($l->t('This application summarizes the workplaces of employees/persons from the calendar.')); ?></b></p>
+		<br/>
+		<p><?php p($l->t('Just put in the calendar a day event with [loc] at the beginning followed by your workplaces.')); ?></p>
+		<p><img src="<?php print_unescaped(image_path('whereami', 'example.png')); ?>" /></p>
+	</div>
+</div>

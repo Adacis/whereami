f53277801d0e91d6a495849489b9f7b2324ca5c0
Benjamin <benjamin@cybercorp.fr>
2022-06-21 22:38:55 +0200

    init

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,5 +1,5 @@
 <ul class="app-navigation">
-    <li class="app-navigation-entry"><span class="navmarg"></span><span>V0.0.6</span></li>
+    <li class="app-navigation-entry"><span class="navmarg"></span><span>V0.0.7</span></li>
 	<li class="app-navigation-entry"><span class="navmarg"></span><input style="width: 200px;" id="dtStart" type="date"/></li>
 	<li class="app-navigation-entry"><span class="navmarg"></span><input style="width: 200px;" id="dtEnd" type="date"/></li>
 	<li class="app-navigation-entry"><span class="navmarg"></span><button style="width: 200px;" class="showbyemployees"><?php p($l->t('By employees'));?></button></li>

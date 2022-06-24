16a5c533e701bf1c6ba466430285726d144ed18d
Antoine Pringalle <58937569+antoinepringalle@users.noreply.github.com>
2022-06-24 17:27:20 +0200

    Tetragraphs (#7)
    * feat(tetragraphs): replaced employees' name by their tetragraph
    
    * feat(tetragraph): moving compute_tetragraph to Event class
    
    Co-authored-by: Antoine Pringalle <a.pringalle-stagiaire@adacis.net>

--- src/js/class/Event.js
+++ src/js/class/Event.js
@@ -28,4 +28,25 @@ export class Events {
     }
     return false
   }
+
+  /**
+   * Takes a name and returns its tetragraph.
+   * "Pierre Martin" will give "PMAR"
+   * "Jean-Paul Dupont" will give "JPDU"
+   * @param {string} name of the user, separated by spaces or '-' for composed names.
+   */
+  compute_tetragraph(name) {
+    const words = name.replace("-", " ").split(" ");  // split by space or -
+    let tetragraph = ""
+    for (let i = 0; i < words.length; i++) {
+      if (i >= words.length - 1) {
+        // takes all first letters from the last word to finish the tetragraph
+        let lettersMissing = Math.max(0, 4 - tetragraph.length)
+        tetragraph += words[i].substring(0, lettersMissing).toUpperCase();
+      } else {
+        tetragraph += words[i][0].toUpperCase();
+      }
+    }
+    return tetragraph;
+  }
 }

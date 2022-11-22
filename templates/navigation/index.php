2c5efae326dec7f1d0e37ea9f1a8464888fa8ea1
Achille Bailly <a.bailly-stagiaire@adacis.net>
2022-11-22 09:50:18 +0100

    Pushed version to 0.0.25

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,5 +1,5 @@
 <ul class="app-navigation">
-	<li class="app-navigation-entry"><span class="navmarg"></span><b>Where am I V 0.0.23</b></li>
+	<li class="app-navigation-entry"><span class="navmarg"></span><b>Where am I V 0.0.25</b></li>
 	<li class="app-navigation-entry">
 		<ul class="app-navigation">
 			<li class="app-navigation-entry"><span class="navmarg icon-contacts-dark"></span><a class="a-entry" id="showByEmployees"><?php p($l->t('Employees')); ?></a>
